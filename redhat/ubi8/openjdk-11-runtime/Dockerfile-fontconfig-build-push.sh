a0bd1ee8ebf4d4ca97250f28099ba2a33e5ed6f7
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-11-11 15:35:04 +0100

    redhat ubi8/openjdk-11-runtime:1.17-5 + fontconfig
    Dockerfile and script

---
+++ redhat/ubi8/openjdk-11-runtime/Dockerfile-fontconfig-build-push.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Build and push my docker file
+#
+VERSION_TAG=${1}
+if [[ "${VERSION_TAG}" == "" ]]; then
+	echo "Version tag (first parameter) cannot be empty"
+else
+	DOCKER_ACCOUNT=fugeritorg
+	DOCKER_FILE=Dockerfile-fontconfig
+	REPO1=${DOCKER_ACCOUNT}/openjdk
+	REPO2=${DOCKER_ACCOUNT}/redhat-ubi8-openjdk
+	echo "VERSION_TAG : ${VERSION_TAG}"
+	TAG1="${REPO1}:redhat-ubi8-openjdk-11-runtime-1.17-5-fontconfig.${VERSION_TAG}"
+	TAG2="${REPO2}:11-runtime-1.17-5-fontconfig.${VERSION_TAG}"
+	echo "REPO1 ${REPO1} TAG1: ${TAG1}"
+	echo "REPO2 ${REPO2} TAG2: ${TAG2}"
+	echo "Dockerfile : ${DOCKER_FILE}"
+	docker build -f Dockerfile-fontconfig -t ${TAG1} -t ${TAG2} .
+	DOCKERBUILD_RES="${?}"
+	if [[ "${DOCKERBUILD_RES}" == "0" ]]; then
+		echo "OK"
+		docker push ${TAG1}
+		docker push ${TAG2}
+	else
+		echo "docker build failed : ${DOCKERBUILD_RES}"
+	fi
+fi
+
